b86e1db9e2f60f547a11a9d3a34570b50ddb9631
squeaky1273 <pjriddell22@gmail.com>
2020-01-28 15:13:34 -0800

    encode and decode

--- Code/bases.py
+++ Code/bases.py
@@ -9,6 +9,11 @@ import string
 # string.ascii_letters is ascii_lowercase + ascii_uppercase
 # string.printable is digits + ascii_letters + punctuation + whitespace
 
+# Variables for string constants
+HEXDIGITS = string.hexdigits
+DIGITS = string.digits
+LOWER = string.ascii_lowercase
+UPPER = string.ascii_uppercase 
 
 def decode(digits, base):
     """Decode given digits in given base to number in base 10.
@@ -18,12 +23,36 @@ def decode(digits, base):
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
     # TODO: Decode digits from binary (base 2)
-    # ...
-    # TODO: Decode digits from hexadecimal (base 16)
-    # ...
-    # TODO: Decode digits from any base (2 up to 36)
-    # ...
+    # exp = exponent  (name of variable)
+    # dec = decimal  (name of variable)
+    if base == 2:
+        exp = 0
+        dec = 0
+        for i in digits[::-1]:
+            dec += int(digits) * 2**exp
+            exp += 1
+        return dec
 
+    # TODO: Decode digits from hexadecimal (base 16)
+    # exp = exponent  (name of variable)
+    # dec = decimal  (name of variable)
+    elif base == 16:
+        exp = 0 
+        dec = 0
+        for i in digits[::-1]:
+            dec += HEXDIGITS.index(i) * 16**exp
+            exp += 1
+        return dec
+
+    # TODO: Decode digits from any base (2 up to 36)
+    # exp = exponent (name of variable)
+    # dec = decimal  (name of variable)
+    exp = 0
+    dec = 0
+    for i in digits[::-1]:
+        dec += (DIGITS + UPPER).index(i) * base**exp
+        exp += 1
+    return dec
 
 def encode(number, base):
     """Encode given number in base 10 to digits in given base.
@@ -35,12 +64,27 @@ def encode(number, base):
     # Handle unsigned numbers only for now
     assert number >= 0, 'number is negative: {}'.format(number)
     # TODO: Encode number in binary (base 2)
-    # ...
-    # TODO: Encode number in hexadecimal (base 16)
-    # ...
-    # TODO: Encode number in any base (2 up to 36)
-    # ...
+    if base == 2:
+        if number > 1:
+            return encode(number//2, base) + str(number % 2)
+        else:
+            return '1'
 
+    # TODO: Encode number in hexadecimal (base 16)
+    elif base == 16:
+        hexdecimal = HEXDIGITS[:16]
+        if number <= 16:
+            return hexdecimal[number]
+        else:
+            return encode(number//16, base) + hexdecimal[number % 16]
+
+    # TODO: Encode number in any base (2 up to 36)
+    """BASE 2-36"""
+    base_digits = (DIGITS + LOWER)[:base]
+    if number < base:
+        return base_digits[number]
+    else:
+        return encode(number//base, base) + base_digits[number % base]
 
 def convert(digits, base1, base2):
     """Convert given digits in base1 to digits in base2.
@@ -59,6 +103,7 @@ def convert(digits, base1, base2):
     # ...
     # TODO: Convert digits from any base to any base (2 up to 36)
     # ...
+    return encode(decode(digits, base1), base2)
 
 
 def main():
