6a26147b989d2d0844a88f1afc4ae8196a5b6890
squeaky1273 <pjriddell22@gmail.com>
2020-03-01 20:32:13 -0800

    sets

--- Code/sets.py
+++ Code/sets.py
@@ -1,63 +1,106 @@
 from hashtable import HashTable
 
 class Set:
-    def _init_(self, elements=None):
+    def __init__(self, elements=None):
         """initialize a new empty set structure, and add each element if a sequence is given"""
-        self.size = 0
-        self.hash = HashTable()
-        pass
+        self.hash_set = HashTable()
+
+        if elements is not None:
+            for element in elements:
+                self.add(element)
+
+    # def is_empty(self):
+    #     return self.size == 0
+
+    def size(self):
+        return self.hash_set.size
 
     def contains(self, element):
         """return a boolean indicating whether element is in this set
-        Running time: O(1); 
+        Running time: O(1); Hash tables work in constant time
         Space(memory): """
-        return self.hash.contains(element) is True
+        return self.hash_set.contains(element)
 
     def add(self, element):
         """add element to this set, if not present already
-        Running time: O(1); 
+        Running time: O(1); Hash tables use key and value pairs
         Space(memory): """
-        if self.hash.contains(element):
-            return('Already in set')
-        else:
-            self.hash.set(element, None)
-            self.size += 1
+        if not self.hash_set.contains(element):
+            self.hash_set.set(element, 1)
 
     def remove(self, element):
         """remove element from this set, if present, or else raise KeyError
-        Running time: O(1); 
+        Running time: O(1); Items in the table are access in constant time
         Space(memory): """
-        if self.hash.contains(element):
-            self.hash.delete(element)
+        if self.hash_set.contains(element):
+            self.hash_set.delete(element)
         else:
             raise ValueError('Item not found: {}'.format(element))
 
     def union(self, other_set):
         """return a new set that is the union of this set and other_set
-        Running time: 
+        Running time: O(n); It takes linear time to get keys
         Space(memory): """
+        #new set
         new_set = Set()
-        ...
+        #old sets
+        this = self.hash_set.keys()
+        other = other_set.hash_set.keys()
+
+        for element in this:
+            new_set.add(element)
+        for element in other:
+            new_set.add(element)
+
         return new_set
 
     def intersection(self, other_set):
         """return a new set that is the intersection of this set and other_set
-        Running time: 
+        Running time: O(n); It takes linear time to get keys
         Space(memory): """
+        # new set
         new_set = Set()
-        ...
+        # old sets
+        # this = self.hash_set.keys()
+        other = other_set.hash_set.keys()
+
+        # for element in this:
+        #     if other_set.contains(element):
+        #         new_set.add(element)
+        for element in other:
+            if self.contains(element):
+                new_set.add(element)
+
         return new_set
 
     def difference(self, other_set):
         """return a new set that is the difference of this set and other_set
-        Running time: 
+        Running time: O(n); key method are conducted in linear time.
         Space(memory): """
+        # new set
         new_set = Set()
-        ...
+        # old sets
+        this = self.hash_set.keys()
+        other = other_set.hash_set.keys()
+
+        for element in this:
+            if other_set.contains(element) is False:
+                new_set.add(element)
+        for element in other:
+            if self.contains(element) is False:
+                new_set.add(element)
+
         return new_set
 
     def is_subset(self, other_set):
         """return a boolean indicating whether other_set is a subset of this set
-        Running time: 
+        Running time: O(n); key method are conducted in linear time.
         Space(memory): """
-        return self.is_subset(other_set) is True
+        # old sets
+        this = self.hash_set.keys()
+        other = other_set.hash_set.keys()
+
+        for element in other:
+            if element not in this:
+                return False
+        return True
--- Code/sets_test.py
+++ Code/sets_test.py
@@ -2,4 +2,71 @@ from sets import Set
 import unittest
 
 class SetsTests(unittest.TestCase):
-    pass
+    
+    def test_init(self):
+        hash_set = Set(['a', 'b', 'c'])
+        assert hash_set.size() == 3
+
+    # def test_is_empty(self):
+    #     set = Set()
+    #     assert set.size == 0
+
+    def test_contains(self):
+        hash_set = Set(['a', 'b', 'c'])
+        assert hash_set.contains('a') == True
+        assert hash_set.contains('b') == True
+        assert hash_set.contains('c') == True
+        assert hash_set.contains('e') == False
+        assert hash_set.contains('f') == False
+
+    def test_add(self):
+        hash_set = Set()
+        hash_set.add('a') == True
+        hash_set.add('b') == True
+        hash_set.add('c') == True
+        assert hash_set.size() == 3
+
+    def test_remove(self):
+        hash_set = Set()
+        hash_set.add('a')
+        hash_set.add('b')
+        assert hash_set.size() == 2
+        hash_set.remove('a')
+        assert hash_set.size() == 1
+
+    def test_union(self):
+        set1 = Set(['a', 'b', 'c'])
+        set2 = Set(['d', 'e', 'f'])
+        set3 = set1.union(set2)
+        assert set3.contains('a') == True
+        assert set3.contains('b') == True
+        assert set3.contains('c') == True
+        assert set3.contains('d') == True
+        assert set3.contains('e') == True
+        assert set3.contains('f') == True
+        assert set3.size() == 6
+
+    def test_intersection(self):
+        set1 = Set(['a', 'b', 'c'])
+        set2 = Set(['c', 'd', 'e'])
+        set3 = set1.intersection(set2)
+        assert set3.contains('c') == True
+        assert set3.size() == 1
+
+    def test_difference(self):
+        set1 = Set(['a', 'b', 'c'])
+        set2 = Set(['c', 'd', 'e'])
+        set3 = set1.difference(set2)
+        assert set3.contains('a') == True
+        assert set3.contains('b') == True
+        assert set3.contains('d') == True
+        assert set3.contains('e') == True
+        assert set3.size() == 4
+
+    def test_is_subset(self):
+        set1 = Set(['a', 'b', 'c'])
+        set2 = Set(['c', 'd'])
+        assert set1.is_subset(set2) == False
+
+if __name__ == '__main__':
+    unittest.main()
