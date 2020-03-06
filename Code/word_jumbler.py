f3266ab0600f27da53012dac8f2977ad2963462d
squeaky1273 <pjriddell22@gmail.com>
2020-03-05 16:19:09 -0800

    added some tests and descriptions for functions

--- Code/word_jumbler.py
+++ Code/word_jumbler.py
@@ -1,13 +1,12 @@
 import sys
-# from hashtable import HashTable
 
 # Dictionary
 class Dict_word_jumbler(object):
     def __init__(self):
-        self.dictionary = self.build_dict()
+        self.dict = self.build_dict()
 
     def build_dict(self):
-        """"Build a dictionary to hold all of the words"""
+        """"Build a dictionary to hold all of the words/letters"""
         dic = {}
         f = open("/usr/share/dict/words", "r")
         word_list = f.readlines()
@@ -17,46 +16,30 @@ class Dict_word_jumbler(object):
             dic[words] = word
         return dic
 
-    def anagram(self, words):
-        """Build an anagram function to unscramble the letters"""
-        answer = {}
+    def unscramble(self, words):
+        """Build a function to unscramble the letters"""
         for word in words:
             word = word.strip().lower()
             word_sorted = ''.join(sorted(word))
-            unscrambled = self.dictionary[word_sorted]
-            answer[unscrambled] = unscrambled
-            print(unscrambled)
-        return answer
-
-# # Hashtable
-# class Hash_word_jumbler(object):
-    # def __init__(self):
-    #     self.hash = self.build_hash()
-
-    # def build_hash(self):
-    #     """"Build a hash to hold all of the words"""
-    #    hashed = {}
-    #     f = open("/usr/share/dict/words", "r")
-    #     word_list = f.readlines()
-    #     for word in word_list:
-    #         word = word.strip().lower()
-    #         words = ''.join(sorted(word))
-    #         hashed[words] = word
-    #     return hashed
-
-    # def anagram(self, words):
-    #     """Build an anagram function to unscramble the letters"""
-    #     for word in words:
-    #         word = word.strip().lower()
-    #         word_sorted = ''.join(sorted(word))
-    #         unscrambled = self.hash[word_sorted]
-    #         print(unscrambled)
+            if word_sorted in self.dict:
+                unscrambled = self.dict[word_sorted]
+                print(unscrambled)
+            else:
+                return None
 
 if __name__ == '__main__':
-    words = ['tefon', 'sokik', 'niumem', 'siconu']
-    jumble = Dict_word_jumbler()
-    jumble.anagram(words)
-
+    # # Cartoon prompt for final jumble:
+    # # "Farley rolled on the barn floor because of his __-______."
+    # words = ['tefon', 'sokik', 'niumem', 'siconu']
+    # jumble = Dict_word_jumbler()
+    # jumble.unscramble(words)
+    
+    # "A bad way for a lawyer to learn the criminal justice system: _____ and _____."
     words = ['laisa', 'laurr', 'bureek', 'prouot']
     jumble = Dict_word_jumbler()
-    jumble.anagram(words)
+    jumble.unscramble(words)
+
+    # # Cartoon prompt for final jumble: "What a dog house is: A ____ ___."
+    # words = ['TARFD', 'JOBUM', 'TENJUK', 'LETHEM']
+    # jumble = Dict_word_jumbler()
+    # jumble.unscramble(words)
